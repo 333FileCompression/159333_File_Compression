410f6d6964ba210b8f8409ff0bdc3304638483fb
333FileCompression <75348477+333FileCompression@users.noreply.github.com>
2021-02-11 16:25:10 +0800

    Add files via upload

---
+++ CompressionSoftware/main.cpp
@@ -0,0 +1,14 @@
+/*
+159.333 Programming Project
+Supervisor: Dr. Andre Barczak
+Author:		Yiqin Wang
+ID:			17280498
+*/
+
+#include"Huffman.h"
+void main() {
+	system("title Compression and Decompression "); // set CMD title
+	system("mode con cols=160 lines=50 "); // set the CMD width and height
+	HuffmanCoding coding; // create a class, it excute the code in constructor in Huffman.h
+	getchar();
+}
